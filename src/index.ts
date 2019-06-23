016131360574aaa9574a18e5f460b3f5a3ef72c9
Tomek <tomek7sa@gmail.com>
2019-06-23 17:27:27 +0200

    Working /close path.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 // lib/app.ts
 import express = require('express');
+import { spawn } from 'child_process';
 
 // Create a new express application instance
 const app: express.Application = express();
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.sendFile('public/index.html', { root: __dirname }, err =>
+    console.log(err)
+  );
+});
+
+app.get('/close', function(req, res) {
+  res.send('Serwer is closing');
+  spawn('shutdown', ['-h', 'now']);
 });
 
 app.listen(3000, function() {
